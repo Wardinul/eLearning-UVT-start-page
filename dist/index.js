2b87a9fa26212050e1411dae9512303fbfe1d7ca
Ardelean Alexandru <alexardelean697@yahoo.com>
2022-12-14 13:17:09 +0200

    sidebar accordion added

--- dist/index.js
+++ dist/index.js
@@ -25,3 +25,19 @@ if (body.classList.contains("dark")) {
 }
 });
 
+
+
+var acc = document.getElementsByClassName('nav-accordion__title');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
+}
